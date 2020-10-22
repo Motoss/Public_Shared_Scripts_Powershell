2895337bad4a0afeb2742f85d2b8857709b24b56
Tom <Motoss@users.noreply.github.com>
2020-10-22 13:11:22 -0700

    first upload of some older scripts
    First upload of some older scripts around for the purpose of desktop configuration automation. Most of these are written to use much older versions of PowerShell so use with caution. Additionally, there may be actual PowerShell commands to complete some of the work that needed to be done previously using other methods. For example, Rename-Computer will also rename a machine whereas before you had to use wmi commands to complete a machine rename.

---
+++ Gather-DomainInfo_v1-2.ps1
@@ -0,0 +1,50 @@
+﻿##################################################################################
+
+<# Code Purpose: To run a health check on Active Directory and output the results to a
+        specified location.     
+
+       All information was changed to protect previous client. Some information may be not be accurate or work as is.
+
+       This Script was written to be used with PowerShell version 2 and 3. 
+  Script Updated on: 9/30/2015                                                  
+      Created By: Thomas Peffers, Senior Systems Engineer / Self Described Code Ninja                 
+#>
+
+##################################################################################
+
+$Location = Read-Host "Please enter location you would like to save the report. Ex 'C:\' without quotes, but include a '\' at the end."
+$PathTest = Test-Path $Location
+If($PathTest -eq $true)
+{
+    $date = Get-Date 
+    $DateFormatted = $date.ToShortDateString().Replace('/','_')
+
+    $AllDomainControllerInfo = Get-ADDomainController -Filter *
+
+    Foreach($DC in $AllDomainControllerInfo)
+    {
+
+        $test = Test-Connection -ComputerName $DC.hostname -Quiet -Count 1
+
+        IF($test -eq $true)
+        {
+            Write-Host "################### $($DC.hostname) #############################"
+
+            $results = dcdiag /s:$($DC.hostname) /v
+
+            $results 
+
+            Write-Host "################### End Report Results: $($DC.hostname) #################################"
+
+            Write-Output "################### $($DC.hostname) #############################" | Out-File "$($Location)DC-Diag-Report_$($DateFormatted)-v.txt" -Append
+
+            $results | Out-File "$($Location)DC-Diag-Report_$($DateFormatted)-v.txt" -Append
+
+            Write-Output "################### End Report Results: $($DC.hostname) #################################" | Out-File "$($Location)DC-Diag-Report_$($DateFormatted)-v.txt" -Append
+
+        }
+
+    }
+
+
+}
